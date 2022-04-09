7d9df967cad672becedf5e4126a17d1e6fd4aa31
Mrunal Sawant <mrunalsawant7@gmail.com>
2022-04-09 20:24:15 +0530

    Unwanted import deleted

--- src/Experience/Renderer.ts
+++ src/Experience/Renderer.ts
@@ -2,7 +2,6 @@
 import * as THREE from 'three';
 import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls';
 import Experience from './Experience';
-// import { Colors } from '../Constant/Constant';
 
 export default class Renderer {
   private experience: Experience;
@@ -27,7 +26,6 @@ export default class Renderer {
     this.instance.setClearColor(0x000000, 0);
     this.instance.setSize(this.experience.config.width, this.experience.config.height);
     this.instance.setPixelRatio(this.experience.config.pixelRatio);
-
     this.instance.physicallyCorrectLights = true;
     this.instance.outputEncoding = THREE.sRGBEncoding;
     this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
